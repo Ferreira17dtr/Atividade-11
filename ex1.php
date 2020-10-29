719c7f2b3740c61a5cd33a3e0b54364cf4e043c2
Professor <Professor@WIN-MP3JADQD58A.local>
2020-10-29 11:12:04 +0000

    Update ex1.php and ex5.php

--- ex1.php
+++ ex1.php
@@ -23,15 +23,31 @@ echo str_replace("Estou a aprender PHP na secundária", "Estou a aprender PHP na
 $cores = array('vermelho ', 'azul ', 'verde ','amarelo ');
 
 echo $cores[1];
-
+echo "<br>";
 ?>
 
 <?php
 
 $a = array("a"=>"maçã","b"=>"banana");
-$b = array("a"=>"kiwi","b"=>"ananás","c"=>"morango");
+$b = array("f"=>"kiwi","d"=>"ananás","c"=>"morango");
 
-echo array_merge($a)
+$resultado = array_merge($a, $b);
+
+foreach ($resultado as $dados) {
+	echo $dados;
+}
+echo "<br>";
+$a = array("a"=>"maçã","b"=>"banana");
+$b = array("c"=>"kiwi","d"=>"ananás","e"=>"morango");
+
+echo "<br>";
+
+$resultado = array_merge($b, $a);
+
+foreach ($resultado as $dados) {
+	echo $dados;
+}
+
 
 ?>
 
